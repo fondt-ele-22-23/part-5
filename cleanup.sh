6a840134af0a124460bf5fcb1b3529c7d52bb85c
Michele Lombardi <michele.lombardi2@unibo.it>
2023-03-13 18:39:52 +0100

    Initial commit

---
+++ cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ `git branch --show-current` = "main" ]; then
+    rm *-sol.ipynb
+    rm *\ -\ sol.ipynb
+    rm pdfs/*-sol.pdf
+    rm pdfs/*\ -\ sol.pdf
+    rm docker-compose.yml
+    rm Dockerfile
+    rm print-pdf.sh
+    rm rise.css
+    rm cleanup.sh
+fi
+
+
